7e95d1676701cd6b6a6e1a0cc75d39cb5a474798
Alexey Sergeev <tender.bumblebee@gmail.com>
2024-04-14 23:59:03 +0300

    add allure report

--- src/test/java/NewOrder.java
+++ src/test/java/NewOrder.java
@@ -1,53 +1,51 @@
-import io.qameta.allure.Description;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 import java.io.File;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
+@RunWith(Parameterized.class)
 public class NewOrder {
 
-    @Before
-    public void setUp() {
+    private final String inputParameter;
+    private final String outputParameter;
+    private final boolean isTruth;
+
+    public NewOrder(boolean isTruth, String inputParameter, String outputParameter) {
+        this.isTruth = isTruth;
+        this.inputParameter = inputParameter;
+        this.outputParameter = outputParameter;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData(){
+        return new Object[][]{
+                {"src/test/resources/newOrderColorBlack.json","track", true},
+                {"src/test/resources/newOrderColorGray.json","track", true},
+                {"src/test/resources/newOrderColorBlackAndGray.json","track", true},
+                {"src/test/resources/newOrderWithoutColor.json","track", true}
+        };
+    }
+
+
+
+    @Test
+    public void newOrderTest(){
+        assertThat(getNewOrder(inputParameter).asString().contains(outputParameter),is(isTruth));
+    }
+
+    public Response getNewOrder(String filePath){
         RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru";
-    }
-
-    @Test
-    @Description("Проверяется создание заказа с цветом Black")
-    public void createOrderWithColorBlack(){
-        File newOrder = new File("src/test/resources/newOrderColorBlack.json");
+        File newOrder = new File(filePath);
         Response response = given().header("Content-type", "application/json").and().body(newOrder).post("/api/v1/orders");
         response.then().statusCode(201);
-        assertTrue(response.asString().contains("track"));
-    }
-    @Test
-    @Description("Проверяется создание заказа с цветом Gray")
-    public void createOrderWithColorGray(){
-        File newOrder = new File("src/test/resources/newOrderColorGray.json");
-        Response response = given().header("Content-type", "application/json").and().body(newOrder).post("/api/v1/orders");
-        response.then().statusCode(201);
-        assertTrue(response.asString().contains("track"));
-    }
-
-    @Test
-    @Description("Проверяется создание заказа с цветами Black и Gray")
-    public void createOrderWithColorBlackAndGray(){
-        File newOrder = new File("src/test/resources/newOrderColorBlackAndGray.json");
-        Response response = given().header("Content-type", "application/json").and().body(newOrder).post("/api/v1/orders");
-        response.then().statusCode(201);
-        assertTrue(response.asString().contains("track"));
-    }
-
-    @Test
-    @Description("Проверяется создание заказа с цветами Black и Gray")
-    public void createOrderWithoutColor(){
-        File newOrder = new File("src/test/resources/newOrderWithoutColor.json");
-        Response response = given().header("Content-type", "application/json").and().body(newOrder).post("/api/v1/orders");
-        response.then().statusCode(201);
-        assertTrue(response.asString().contains("track"));
+        return response;
     }
 
 
--- src/test/java/OrderList.java
+++ src/test/java/OrderList.java
@@ -1,2 +1,28 @@
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.*;
+
+
 public class OrderList {
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru";
+    }
+
+    @Test
+    @Description("Проверяется, что в ответе ручки списка заказов пришёл список заказов")
+    public void getOrderListTest(){
+
+        Response response = given().header("Content-type", "application/json").and().get("/api/v1/orders");
+        System.out.println(response.body().asString());
+        response.then().assertThat().body("orders",notNullValue()).and().statusCode(200);
+    }
+
 }
